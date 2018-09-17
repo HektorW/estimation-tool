2e94d1b6c4d4ce1c67d8d6005012560cd42d7ebf
Hektor Wallin <hektorw@gmail.com>
2018-09-17 21:01:18 +0200

    Working estimation tool

---
+++ client/main.js
@@ -0,0 +1,35 @@
+const estimateInputEl = document.querySelector('[data-input-estimate]')
+const multiplyInputEl = document.querySelector('[data-input-multiply]')
+const addInputEl = document.querySelector('[data-input-add]')
+const resultEl = document.querySelector('[data-result]')
+
+const updateResults = () => {
+  const estimate = parseFloat(estimateInputEl.value || 0)
+  const multiply = parseFloat(multiplyInputEl.value || 0)
+  const addHours = parseFloat(addInputEl.value || 0)
+
+  resultEl.innerHTML = roundResult(estimate * multiply + addHours)
+}
+
+const resizeInput = event => {
+  const target = event.target
+  const span = target.parentNode.querySelector('.hidden')
+  span.textContent = target.value
+  target.style.width = span.offsetWidth + 'px'
+}
+
+const roundResult = value =>
+  Number.isNaN(value)
+    ? 0
+    : isInteger(value)
+      ? value
+      : (Math.round(value * 4) / 4).toFixed(2)
+
+const isInteger = value => Math.round(value) === value
+;[estimateInputEl, multiplyInputEl, addInputEl].forEach(el => {
+  el.addEventListener('input', updateResults)
+  el.addEventListener('input', resizeInput)
+  resizeInput({ target: el })
+})
+
+estimateInputEl.focus()
